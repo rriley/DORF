b068e65a1447f3595b3a3984a541e2dd15ab99eb
Ryan Riley <ryan.riley@qu.edu.qa>
2011-11-07 17:45:43 +0300

    An attempt at an ubuntu 10.04 port.  Things work, but it isn't clean or complete

---
+++ dorf/ubuntu1004/kstructs.py
@@ -0,0 +1,157 @@
+import os, sys
+from ctypes import *
+
+class list_head(Structure):
+	_fields_ = [	('next', c_uint),
+			('prev', c_uint)
+		   ]
+
+        def remove(self, dm):
+                prev = list_head()
+                next = list_head()
+
+                if self.prev != 0:
+                        dm.read(prev, self.prev)
+                        prev.next = self.next
+                        dm.write(prev, self.prev)
+
+                if self.next != 0:
+                        dm.read(next, self.next)
+                        next.prev = self.prev
+                        dm.write(next, self.next)
+
+class hlist_node(Structure):
+	_fields_ = [	('next', c_uint),
+			('pprev', c_uint)
+		   ]
+
+	def remove(self, dm):
+		pprev = hlist_node()
+		next = hlist_node()
+
+		if self.pprev != 0:
+			dm.read(pprev, self.pprev)
+			pprev.next = self.next
+			dm.write(pprev, self.pprev)
+
+		if self.next != 0:
+			dm.read(next, self.next)
+			next.pprev = self.pprev
+			dm.write(next, self.next)
+
+class pid(Structure):
+	list_offset = 36
+
+	_fields_ = [	('junk1', c_char * 28),
+			('nr', c_int),
+			('ns', c_void_p),
+			('pid_chain', hlist_node)
+		   ]
+
+	def __str__(self):
+		return "PID: " + str(self.nr)
+
+	def next(self):
+		return self.pid_chain.next
+
+	def pprev(self):
+		return self.pid_chain.pprev
+
+	def compare(self, thing):
+		return thing-self.nr
+
+class task_struct(Structure):
+	list_offset = 472
+
+	_fields_ = [ 	('junk1',c_char * 472), 
+			('tasks',list_head), 
+			('junk2',c_char * 52),
+			('pid', c_int)
+		   ]
+	def prev(self):
+		return self.tasks.prev - self.list_offset
+
+	def next(self):
+		return self.tasks.next - self.list_offset
+
+class qstr(Structure):
+	_fields_ = [	('hash', c_int),
+			('len', c_int),
+			('name', c_void_p)
+		   ]
+
+	def get_string(self, dm):
+		return dm.read_bytes(self.name, self.len)		
+
+class dentry(Structure):
+	list_offset = 20
+	subdirs_offset = 60
+	child_offset = 52
+
+	_fields_ = [	('d_count', c_uint),
+			('d_flags', c_uint),
+			('junk1', c_char*8),
+			('d_inode', c_void_p),
+			('d_hash', hlist_node),
+			('d_parent', c_void_p),
+			('d_name', qstr),
+			('junk2', c_char*8),
+			('d_child', list_head),
+			('d_subdirs', list_head),
+			('junk3', c_char*28),
+			('d_iname', c_char*32)
+		   ]
+
+	dm = None
+
+	def set_dm(self, dm):
+		self.dm = dm
+
+	def __str__(self):
+		return self.d_name.get_string(self.dm)
+
+	def next(self):
+		return self.d_hash.next
+
+	def pprev(self):
+		return self.d_hash.pprev
+
+	def compare(self, thing):
+		me = self.__str__()
+		return (thing > me) - (thing < me)
+
+class buffer_head(Structure):
+	_fields_ = [	('b_state', c_uint),
+			('junk1', c_uint),
+			('b_page', c_uint),
+			('b_blocknr', c_uint64),
+			('b_size', c_uint),
+			('b_data', c_uint),
+			('junk2', c_uint*6),
+			('b_count', c_uint)
+		   ]
+	
+	def __str__(self):
+		s = ""
+
+		for i in range(0,2):
+			s += ("junk1["+str(i)+"]: " + hex(self.junk1[i]) + "\n")
+
+		s += "b_page: " + hex(self.b_page) + "\n"
+		s += "b_blocknr: " + hex(self.b_blocknr) + "\n"
+		s += "b_size: " + hex(self.b_size) + "\n"
+		s += "b_data: " + hex(self.b_data) + "\n"
+
+		for i in range(0,6):
+			s += ("junk2["+str(i)+"]: " + hex(self.junk2[i]) + "\n")
+
+		s += "b_count: " + hex(self.b_count) + "\n"
+
+		return s
+
+class dir_entry(Structure):
+	_fields_ = [	('inode', c_uint),
+			('rec_len', c_short),
+			('name_len', c_byte),
+			('file_type', c_byte)
+		   ]
---
+++ dorf/ubuntu1004/memreader.py
@@ -0,0 +1,55 @@
+import os, sys
+from ctypes import *
+
+class DevMemReader:
+	def __init__(self, symbol_finder):
+		self.sf = symbol_finder;
+		self.fd = open("/dev/mem","r+b")
+		self.swapper_pg_dir = self.sf.find("swapper_pg_dir");
+
+	def _virt_to_phys(self, addr):
+		pte = c_int()
+
+		# Find the initial pg_dir for this address
+		next = (self.swapper_pg_dir - 0xc0000000) | ((addr >> 20) & 0xffc)
+		self._read(pte, next)
+
+		# If this isn't a large page, then go one level deeper for the pte
+		if (pte.value & 0x80) == 0:
+			next = (pte.value & 0xfffff000) | ((addr >> 10) & 0xffc);
+			self._read(pte, next)
+
+		return (pte.value & 0xffc00000) | (addr & 0x003fffff)
+
+	# read from a physical address
+	def _read(self, dest, addr):
+		try:
+			self.fd.seek(addr)
+			self.fd.readinto(dest)
+		except:
+		    print "Some error"
+
+
+	# read from a virtual address into a ctype structure
+	def read(self, dest, addr):
+		self._read(dest, self._virt_to_phys(addr))
+
+	def _write(self, src, addr):
+		self.fd.seek(addr)
+		self.fd.write(src)
+
+	def write(self, src, addr):
+		self._write(src, self._virt_to_phys(addr))
+
+	# read a 4-byte value.  Do with it what you will...
+	def read_int(self, addr):
+		tmp = c_uint()
+		self.read(tmp,addr)
+		return tmp.value
+
+	def _read_bytes(self, addr, num):
+		self.fd.seek(addr)
+		return self.fd.read(num)
+
+	def read_bytes(self, addr, num):
+		return self._read_bytes(self._virt_to_phys(addr), num)
