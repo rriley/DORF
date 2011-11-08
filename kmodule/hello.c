089fb9f5ca7bb589fd7bb44b6567afe17051521e
Ryan Riley <ryan.riley@qu.edu.qa>
2011-11-08 10:38:58 +0000

    Modified kernel module to handle better detection of bh_lrus offset

--- kmodule/hello.c
+++ kmodule/hello.c
@@ -6,24 +6,33 @@
 #include <linux/sched.h>
 #include <linux/proc_fs.h>
 #include <linux/buffer_head.h>
+#include <linux/version.h>
+
+/* Some code to handle finding the offset to bh_lrus */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,0)
 
 #define BH_LRU_SIZE	8
-
 struct bh_lru {
  	struct buffer_head *bhs[BH_LRU_SIZE];
 };
-
 DEFINE_PER_CPU(struct bh_lru, bob_lrus);
+#endif
 
 int init_module(void)
 {
 	struct task_struct *bob = (struct task_struct *)NULL;
 	struct proc_inode *fred = (struct proc_inode *) NULL;
 	struct dentry *a = (struct dentry *) NULL;
-	int per_cpu__bh_lrus = NULL;
+
+	#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,0)
+	unsigned int bh_offset = (unsigned int)&get_cpu_var(bob_lrus) - (unsigned int)&bob_lrus;
+	#else
+	unsigned int bh_offset = (unsigned int)&per_cpu(bh_lrus, 0) - (unsigned int)&(per_cpu__bh_lrus);
+	#endif
 
 	printk(KERN_INFO "-------------------\n");
 	printk(KERN_INFO "task_struct list_offset: %d\n", (int)&(bob->tasks));
+	printk(KERN_INFO "offset of bh_lrus: %08x\n", bh_offset);
 
 	printk(KERN_INFO "---\n");	
 	printk(KERN_INFO "tasks: %d\n", (int)&(bob->tasks));
@@ -33,10 +42,6 @@ int init_module(void)
 	printk(KERN_INFO "vfs_inode: %d\n", (int)&(fred->vfs_inode));
 	printk(KERN_INFO "dentry.d_subdirs: %d\n", (int)&(a->d_subdirs));
 	printk(KERN_INFO "buffer_head size: %d\n", sizeof(struct buffer_head));
-	//printk(KERN_INFO "offset of bh_lrus: %08x\n", (unsigned int)&per_cpu(bh_lrus, 0) - (unsigned int)&(per_cpu__bh_lrus));
-	printk(KERN_INFO "&bob_lrus: %08x\n", (unsigned int)&bob_lrus);
-	printk(KERN_INFO "&get_cpu_var(bob_lrus): %08x\n", (unsigned int)&get_cpu_var(bob_lrus));
-	printk(KERN_INFO "offset of bh_lrus: %08x\n", (unsigned int)&get_cpu_var(bob_lrus) - (unsigned int)&bob_lrus);
 
 	/* 
 	 * A non 0 return means init_module failed; module can't be loaded. 
